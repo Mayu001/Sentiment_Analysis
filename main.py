442c3606211a47d7f01a6e96c4b06d08d3e871ff
Abhishek257 <abhichauhanm@gmail.com>
2018-02-09 00:05:08 +0530

    saare databases added,,,no need of any other databases

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-samplestring='I am a good boy'
+from collections import Counter
+samplestring='I am a good boy I amm good good am boy girl'
+#unigram bigrams trigrams
 unigrams=samplestring.split(" ")
 bigrams=[]
 trigrams=[]
 i=0
 while i<len(unigrams)-1:
-    bigrams.append(unigrams[i]+" "+unigrams[i+1])
+    bigrams.append(unigrams[i] + unigrams[i+1])
     i+=1
 i=0
 while i<len(unigrams)-2:
-    trigrams.append(unigrams[i] + " " + unigrams[i + 1] + " " + unigrams[i + 2])
+    trigrams.append(unigrams[i] + unigrams[i + 1] + unigrams[i + 2])
     i+=1
-for i in trigrams:
-    print(i)
+#frequent items
+#unigram
+most_common_unigrams= [word for word, word_count in Counter(unigrams).most_common(3)]
+#bigram
+most_common_bigrams= [word for word, word_count in Counter(bigrams).most_common(3)]
+#trigram
+most_common_trigrams= [word for word, word_count in Counter(trigrams).most_common(3)]
+print(most_common_unigrams,most_common_bigrams,most_common_trigrams)
