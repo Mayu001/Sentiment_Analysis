484b44207cd0e37d5fd7a36c6398be520bf026e9
Mayank Mohit <mayank.mohit1122@gmail.com>
2018-02-09 15:53:11 +0530

    DOne for negative

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from collections import Counter
 from nltk.tokenize import word_tokenize
-samplestring='I am a good boy I amm good good am boy girl'
-samplestring1='I am a bad boy I amm bad bad am boy girl'
 #unigram bigrams trigrams
 
 for line in open("positive_sentences.txt", encoding="utf-8").read().split("\n")[:-1]:
@@ -23,4 +21,25 @@ most_common_posBigrams= [word for word, word_count in Counter(posBigrams).most_c
 most_common_posTrigrams= [word for word, word_count in Counter(posTrigrams).most_common(3)] #trigram
 print(most_common_posUnigrams,most_common_posBigrams,most_common_posTrigrams)
 
+for line in open("negative_sentences.txt", encoding="utf-8").read().split("\n")[:-1]:
+    #negUnigrams=word_tokenize(line)
+    negUnigrams=line.split(" ")
+    negBigrams=[]
+    negTrigrams=[]
+    i=0
+    while i<len(negUnigrams)-1:
+        negBigrams.append(negUnigrams[i] + negUnigrams[i+1])
+        i+=1
+    i=0
+    while i<len(negUnigrams)-2:
+        negTrigrams.append(negUnigrams[i] + negUnigrams[i + 1] + negUnigrams[i + 2])
+        i+=1
+#frequent items
+#unigram
+most_common_negUnigrams= [word for word, word_count in Counter(negUnigrams).most_common(3)]
+#bigram
+most_common_negBigrams= [word for word, word_count in Counter(negBigrams).most_common(3)]
+#trigram
+most_common_negTrigrams= [word for word, word_count in Counter(negTrigrams).most_common(3)]
+print(most_common_negUnigrams,most_common_negBigrams,most_common_negTrigrams)
 
