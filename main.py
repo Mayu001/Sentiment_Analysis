e2e84d861ac7d28983d319ec8f43ac37e98702db
Mayank Mohit <mayank.mohit1122@gmail.com>
2018-02-09 18:19:36 +0530

    Everything is working till Creating and and stroing Gramss

--- main.py
+++ main.py
@@ -1,45 +1,67 @@
 from collections import Counter
-from nltk.tokenize import word_tokenize
+#from nltk.tokenize import word_tokenize
 #unigram bigrams trigrams
+posUnigram=[]
+posBigram=[]
+posTrigram=[]
 
 for line in open("positive_sentences.txt", encoding="utf-8").read().split("\n")[:-1]:
     #posUnigrams=word_tokenize(line)
-    posUnigrams=line.split(" ")
-    posBigrams=[]
-    posTrigrams=[]
+    pU=line.split(" ")
+    pB=[]
+    pT=[]
     i=0
-    while i<len(posUnigrams)-1:
-        posBigrams.append(posUnigrams[i] + posUnigrams[i+1])
+    while i<len(pU)-1:
+        pB.append(pU[i] + pU[i+1])
         i+=1
     i=0
-    while i<len(posUnigrams)-2:
-        posTrigrams.append(posUnigrams[i] + posUnigrams[i + 1] + posUnigrams[i + 2])
+    while i<len(pU)-2:
+        pT.append(pU[i] + pU[i + 1] + pU[i + 2])
         i+=1
+    posUnigram=posUnigram+pU
+    posBigram=posBigram+pB
+    posTrigram=posTrigram+pT
 #frequent items
-most_common_posUnigrams= [word for word, word_count in Counter(posUnigrams).most_common(3)]  #unigram
-most_common_posBigrams= [word for word, word_count in Counter(posBigrams).most_common(3)] #bigram
-most_common_posTrigrams= [word for word, word_count in Counter(posTrigrams).most_common(3)] #trigram
+most_common_posUnigrams= [word for word, word_count in Counter(posUnigram).most_common(1000)]  #unigram
+import pickle
+with open("common_posUnigrams.pkl", "wb") as a1:
+    pickle.dump(most_common_posUnigrams, a1)
+most_common_posBigrams= [word for word, word_count in Counter(posBigram).most_common(1000)] #bigram
+with open("most_common_posBigrams.pkl", "wb") as a1:
+    pickle.dump(most_common_posBigrams, a1)
+most_common_posTrigrams= [word for word, word_count in Counter(posTrigram).most_common(1000)] #trigram
+with open("most_common_posTrigrams.pkl", "wb") as a1:
+    pickle.dump(most_common_posTrigrams, a1)
 print(most_common_posUnigrams,most_common_posBigrams,most_common_posTrigrams)
-
+print('\n\n')
+negUnigram=[]
+negBigram=[]
+negTrigram=[]
 for line in open("negative_sentences.txt", encoding="utf-8").read().split("\n")[:-1]:
     #negUnigrams=word_tokenize(line)
-    negUnigrams=line.split(" ")
-    negBigrams=[]
-    negTrigrams=[]
+    nU=line.split(" ")
+    nB=[]
+    nT=[]
     i=0
-    while i<len(negUnigrams)-1:
-        negBigrams.append(negUnigrams[i] + negUnigrams[i+1])
+    while i<len(nU)-1:
+        nB.append(nU[i] + nU[i+1])
         i+=1
     i=0
-    while i<len(negUnigrams)-2:
-        negTrigrams.append(negUnigrams[i] + negUnigrams[i + 1] + negUnigrams[i + 2])
+    while i<len(nU)-2:
+        nT.append(nU[i] + nU[i + 1] + nU[i + 2])
         i+=1
+    negUnigram=negUnigram+nU
+    negBigram=negBigram+nB
+    negTrigram=negTrigram+nT
 #frequent items
-#unigram
-most_common_negUnigrams= [word for word, word_count in Counter(negUnigrams).most_common(3)]
-#bigram
-most_common_negBigrams= [word for word, word_count in Counter(negBigrams).most_common(3)]
-#trigram
-most_common_negTrigrams= [word for word, word_count in Counter(negTrigrams).most_common(3)]
+most_common_negUnigrams= [word for word in Counter(negUnigram).most_common(1000)]  #unigram
+with open("most_common_negUnigrams.pkl", "wb") as a1:
+    pickle.dump(most_common_negUnigrams, a1)
+most_common_negBigrams= [word for word, word_count in Counter(negBigram).most_common(1000)] #bigram
+with open("most_common_negBigrams.pkl", "wb") as a1:
+    pickle.dump(most_common_negBigrams, a1)
+most_common_negTrigrams= [word for word, word_count in Counter(negTrigram).most_common(1000)] #trigram
+with open("most_common_negTrigrams.pkl", "wb") as a1:
+    pickle.dump(most_common_negTrigrams, a1)
 print(most_common_negUnigrams,most_common_negBigrams,most_common_negTrigrams)
 
