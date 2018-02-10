e73c5bf807b277c0d19e5b6ae09741b607780950
Mayank Mohit <mayank.mohit1122@gmail.com>
2018-02-10 14:42:46 +0530

    list bn gai finally

--- training.py
+++ training.py
@@ -1,8 +1,20 @@
 import pickle
 from nltk.tokenize import word_tokenize
-univtrained={}
+univtrained=[]
+posgram=[]
+neggram=[]
 posunigrams = []
 negunigrams = []
+for word in open("positivelexicon.txt", encoding="utf-8").read().split("\n")[:-1]:
+    abc = word_tokenize(word)
+    for a in abc:
+        if a.isalnum():
+            posgram.append(a)
+for word in open("negativelexicon.txt", encoding="utf-8").read().split("\n")[:-1]:
+    abc = word_tokenize(word)
+    for a in abc:
+        if a.isalnum():
+            neggram.append(a)
 with (open("common_posUnigrams.pkl", "rb")) as openfile:
     while True:
         try:
@@ -69,10 +81,14 @@ for line in open("positive_sentences.txt", encoding="utf-8").read().split("\n")[
     negative = 0
     abcd=[]
     word =""
-    if any(word in unigram for word in posunigrams):
-      positive+=1
-    if any(word in unigram for word in negunigrams):
-      negative-=1
+    for word in unigram:
+        positive += posunigrams.count(word)
+    for word in unigram:
+        negative += negunigrams.count(word)
+    # if any(word in unigram for word in posunigrams):
+    #   positive+=1
+    # if any(word in unigram for word in negunigrams):
+    #   negative-=1
     if positive >negative:
         abcd.append(positive)
     else:
@@ -80,26 +96,114 @@ for line in open("positive_sentences.txt", encoding="utf-8").read().split("\n")[
     positive=0
     negative=0
     word=""
-    if any(word in bigram for word in posbigrams):
-      positive+=1
-    if any(word in bigram for word in negbigrams):
-      negative-=1
+    for word in bigram:
+        positive += posbigrams.count(word)
+    for word in bigram:
+        negative += negbigrams.count(word)
+    # if any(word in bigram for word in posbigrams):
+    #   positive+=1
+    # if any(word in bigram for word in negbigrams):
+    #   negative-=1
     if positive >negative:
         abcd.append(positive)
     else:
         abcd.append(negative)
     positive = 0
     negative = 0
-    word = ""
-    if any(word in trigram for word in postrigrams):
-      positive+=1
-    if any(word in trigram for word in negtrigrams):
-      negative-=1
+    for word in trigram:
+        positive += postrigrams.count(word)
+    for word in trigram:
+        negative += negtrigrams.count(word)
+    # if any(word in trigram for word in postrigrams):
+    #   positive+=1
+    # if any(word in trigram for word in negtrigrams):
+    #   negative-=1
     if positive >negative:
         abcd.append(positive)
     else:
         abcd.append(negative)
-    print(abcd)
-    univtrained
+    for word in unigram:
+        positive += posgram.count(word)
+    for word in trigram:
+        negative += neggram.count(word)
+    if positive>negative:
+        abcd.append(positive)
+    else:
+        abcd.append(negative)
+    univtrained.append(abcd)
+for line in open("negative_sentences.txt", encoding="utf-8").read().split("\n")[:-1]:
+    positive = 0
+    abc = word_tokenize(line)
+    unigram = []
+    for a in abc:
+        if a.isalnum():
+            unigram.append(a)
+    bigram=[]
+    trigram=[]
+    trainedlist=[]
+    i=0
+    while i<len(unigram)-1:
+        bigram.append(unigram[i] + unigram[i+1])
+        i+=1
+    i=0
+    while i<len(unigram)-2:
+        trigram.append(unigram[i] + unigram[i + 1] + unigram[i + 2])
+        i+=1
+    #check for unigrams
+
+    positive = 0
+    negative = 0
+    abcd=[]
+    word =""
+    for word in unigram:
+        positive += posunigrams.count(word)
+    for word in unigram:
+        negative += negunigrams.count(word)
+    # if any(word in unigram for word in posunigrams):
+    #   positive+=1
+    # if any(word in unigram for word in negunigrams):
+    #   negative-=1
+    if positive >negative:
+        abcd.append(positive)
+    else:
+        abcd.append(negative)
+    positive=0
+    negative=0
+    word=""
+    for word in bigram:
+        positive += posbigrams.count(word)
+    for word in bigram:
+        negative += negbigrams.count(word)
+    # if any(word in bigram for word in posbigrams):
+    #   positive+=1
+    # if any(word in bigram for word in negbigrams):
+    #   negative-=1
+    if positive >negative:
+        abcd.append(positive)
+    else:
+        abcd.append(negative)
+    positive = 0
+    negative = 0
+    for word in trigram:
+        positive += postrigrams.count(word)
+    for word in trigram:
+        negative += negtrigrams.count(word)
+    # if any(word in trigram for word in postrigrams):
+    #   positive+=1
+    # if any(word in trigram for word in negtrigrams):
+    #   negative-=1
+    if positive >negative:
+        abcd.append(positive)
+    else:
+        abcd.append(negative)
+    for word in unigram:
+        positive += posgram.count(word)
+    for word in trigram:
+        negative += neggram.count(word)
+    if positive>negative:
+        abcd.append(positive)
+    else:
+        abcd.append(negative)
+    univtrained.append(abcd)
 print(univtrained)
 
