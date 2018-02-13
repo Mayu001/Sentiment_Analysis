58d0320f4f88f30ef197ab3b4bbc89f5065d36c1
Mayank Mohit <mayank.mohit1122@gmail.com>
2018-02-13 23:08:10 +0530

    Decision Tree trained

--- training.py
+++ training.py
@@ -5,6 +5,7 @@ from sklearn import svm,datasets
 from sklearn.naive_bayes import GaussianNB
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import BernoulliNB
+from sklearn import tree
 univtrained=[]
 univnature=[]
 posgram=[]
@@ -222,5 +223,8 @@ with open("Trained_MultinomialNB.pkl","wb") as a:
 clf=BernoulliNB().fit(univtrained,univnature)
 with open("Trained_bernoulli.pkl", "wb") as a:
     pickle.dump(clf, a)
-    print("created")
+DTclf = tree.DecisionTreeClassifier()
+DTclf=DTclf.fit(univtrained,univnature)
+with open("Trained_decisionTree.pkl", "wb") as a:
+    pickle.dump(DTclf, a)
 print("training done")
